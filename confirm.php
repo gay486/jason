3134b59b48b90b0d5eedba67d29f0b64b043a3bd
gay486 <akinizi486@gmail.com>
2021-01-19 17:28:59 +0900

    update

---
+++ confirm.php
@@ -0,0 +1,40 @@
+<?
+    include_once('header.php');
+?>
+
+
+<main id="confirm">
+    <section id="section1">
+        <div class="wrap">
+            <div class="gap">
+                <div class="container">
+                    <div class="title">
+                        <h2>여행문의</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="section2">
+    <div class="wrap">
+        <div class="gap">
+            <div class="container">
+                <!--갤러리-->
+            </div>
+        </div>
+    </div>
+    </section>
+    <section id="section3">
+    <div class="wrap">
+        <div class="gap">
+            <div class="container">
+                <!--슬라이드-->
+            </div>
+        </div>
+    </div>
+    </section>
+</main>
+
+<?
+    include_once('footer.php');
+?>
---
+++ main1-9.php
@@ -0,0 +1,40 @@
+<?
+    include_once('header.php');
+?>
+
+
+<main id="main1-9">
+    <section id="section1">
+        <div class="wrap">
+            <div class="gap">
+                <div class="container">
+                    <div class="title">
+                        <h2>태교여행</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="section2">
+    <div class="wrap">
+        <div class="gap">
+            <div class="container">
+                <!--갤러리-->
+            </div>
+        </div>
+    </div>
+    </section>
+    <section id="section3">
+    <div class="wrap">
+        <div class="gap">
+            <div class="container">
+                <!--슬라이드-->
+            </div>
+        </div>
+    </div>
+    </section>
+</main>
+
+<?
+    include_once('footer.php');
+?>
